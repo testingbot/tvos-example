e54104cdbcadb417daeca714e1178a6cfdff3779
Jochen Delabie <jochen@testingbot.com>
2025-01-10 10:43:59 +0100

    Initial Commit

---
+++ tvos-example/tvos_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvos_exampleApp.swift
+//  tvos-example
+//
+//  Created by Jochen Delabie on 10/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct tvos_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tvos-exampleTests/tvos_exampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  tvos_exampleTests.swift
+//  tvos-exampleTests
+//
+//  Created by Jochen Delabie on 10/01/2025.
+//
+
+import Testing
+@testable import tvos_example
+
+struct tvos_exampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
